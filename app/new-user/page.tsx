ae3230cd922b15e1acda144efbdbf4f15bc29544
ThomasxLunke <thomas-sidambarom@hotmail.com>
2025-05-01 09:56:27 +0200

    :construction:

--- app/new-user/page.tsx
+++ app/new-user/page.tsx
@@ -1,5 +1,33 @@
 import React from 'react'
+import { prisma } from '@/utils/db'
+import { currentUser } from '@clerk/nextjs'
+import { redirect } from 'next/navigation'
 
-export default function page() {
-  return <div>new-user</div>
+const createNewUser = async () => {
+  const user = await currentUser()
+  const match = await prisma.user.findUnique({
+    where: {
+      clerkId: user?.id ?? '',
+    },
+  })
+  if (!match) {
+    await prisma.user.create({
+      data: {
+        clerkId: user?.id ?? '',
+        email: user?.emailAddresses[0].emailAddress ?? '',
+      },
+    })
+  }
+
+  redirect('/journal')
+}
+
+export default async function Page() {
+  await createNewUser()
+
+  return (
+    <div>
+      <div>lol</div>
+    </div>
+  )
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,10 @@
 import Link from 'next/link'
 import React from 'react'
+import { auth } from '@clerk/nextjs'
 
 export default function page() {
+  const { userId } = auth()
+  const href = userId ? '/journal' : '/new-u ser'
   return (
     <div className="w-screen h-screen bg-black flex justify-center items-center text-white">
       <div className="w-full max-w-[600px] mx-auto">
@@ -11,7 +14,7 @@ export default function page() {
           you have to do is be honest.
         </p>
         <div>
-          <Link href="/journal">
+          <Link href={href}>
             <button className="bg-blue-600 px-4 py-2 rounded-lg text-xl">
               get started
             </button>
